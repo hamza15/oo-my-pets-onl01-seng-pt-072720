d15f607b176ee998bd32cadc7d9505e4eb81e8d1
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-27 05:15:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,7 +57,12 @@ class Owner
   end
   
   def sell_pets
-    
+     pets = self.dogs + self.cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
   end
   
   def list_pets
